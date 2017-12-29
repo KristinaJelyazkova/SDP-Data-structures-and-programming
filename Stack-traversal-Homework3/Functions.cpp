5cefe15fc902118a2c1830720f241d5f266c5b86
KristinaJelyazkova <thesecretgirlbls@abv.bg>
2017-12-29 13:58:54 +0200

    Add files via upload

--- Stack-traversal-Homework3/Functions.cpp
+++ Stack-traversal-Homework3/Functions.cpp
@@ -7,149 +7,90 @@
 
 //  ¿  ƒ¿ —» —⁄«ƒ¿ƒ≈Ã HELP »“≈–¿“Œ– ¬⁄“–≈ ¬⁄¬ ‘”Õ ÷»ﬂ“¿???
 
-template<typename T>
-int count(BaseIterator<T>& it, const T& x) {
+template<class Iterator, class T>
+int count(Iterator it, T x) {
 	int counter = 0;
 	while (it.more()) {
-		if (it.getNext() == x) counter++;
+		if (it.getNext() == x) {
+			counter++;
+		}
 	}
 	return counter;
 }
 
-template<typename T>
-void map(BaseIterator<T>& it, T(*function) (const T &)) {
+template<class Iterator, class T>
+void map(Iterator it, T(*f) (T&)) {
 	while (it.more()) {
-		T &current = it.getNext();
-		current = function(current);
+		T& tmp = it.getNext();
+		tmp = f(tmp);
 	}
 }
 
-template<typename T>
-bool duplicates(ListIterator<T> it) {
+template<class Iterator, class T>
+bool duplicates(Iterator it) {
 	while (it.more()) {
 		T current = it.getNext();
-		ListIterator<T> help = it;
+		Iterator help = it;
 		while (help.more()) {
-			if (current == help.getNext()) return true;
+			if (current == help.getNext()) {
+				return true;
+			}
 		}
 	}
 	return false;
 }
 
-template<typename T>
-bool duplicatesArray(ArrayIterator<T> it) {
-	while (it.more()) {
-		T current = it.getNext();
-		ArrayIterator<T> help = it;
-		while (help.more()) {
-			if (current == help.getNext()) return true;
-		}
-	}
-	return false;
-}
-
-template<typename T>
-bool duplicatesString(StringIterator<T> it) {
-	while (it.more()) {
-		T current = it.getNext();
-		StringIterator<T> help = it;
-		while (help.more()) {
-			if (current == help.getNext()) return true;
-		}
-	}
-	return false;
-}
-
-template<typename T>
-bool isSorted(BaseIterator<T>& it) {
-	T current = 0 , next = 0;
-	if (it.more())
-		current = it.getNext();
-	else return true;
-	if (it.more())
-		next = it.getNext();
-	else return true;
+template<class Iterator, class T>
+bool isSorted(Iterator it) {
+	T current, next;
 	int flag = 0;
-	if (current < next) 
-		flag = 1;
-	if (current > next)
+	if (!it.more()) {
+		return true;
+	}
+	else {
+		current = it.getNext();
+	}
+	if (!it.more()) {
+		return true;
+	}
+	else {
+		next = it.getNext();
+	}
+	if (current < next) {
 		flag = -1;
+	}
+	if (current > next) {
+		flag = 1;
+	}
 	while (it.more()) {
 		current = next;
 		next = it.getNext();
 		if (flag == 0) {
-			if (current < next)
-				flag = 1;
-			if (current > next)
+			if (current < next) {
 				flag = -1;
+			}
+			if (current > next) {
+				flag = 1;
+			}
 		}
-		if ( (current > next && flag == 1) || (current < next && flag == -1) )
+		if ((current > next && flag == -1) || (current < next && flag == 1)) {
 			return false;
+		}
 	}
 	return true;
 }
 
-template<typename T>
-bool palindrom(ListIterator<T> it) {
+template<class Iterator, class T>
+bool palindrom(Iterator it) {
 	std::vector<T> save;
-	ListIterator<T> help = it;
-	bool flag = 0;
+	Iterator help = it;
 	while (help.more()) {
-		save.push_back(it.getNext());
-		//std::cout << save.back() << " ";
-		help.getNext();
-		if(help.more()) help.getNext();
-		else flag = 1;
+		save.push_back(help.getNext());
 	}
-	if (flag == 1) save.pop_back();
 	while (it.more()) {
-		//std::cout << save.back() << " " << it.getNext() << std::endl;
-		if (save.back() != it.getNext())
+		if (save.back() != it.getNext()) {
 			return false;
-		save.pop_back();
-	}
-	return true;
-}
-
-template<typename T>
-bool palindromArray(ArrayIterator<T> it) {
-	std::vector<T> save;
-	ArrayIterator<T> help = it;
-	bool flag = 0;
-	while (help.more()) {
-		save.push_back(it.getNext());
-		//std::cout << save.back() << " ";
-		help.getNext();
-		if (help.more()) help.getNext();
-		else flag = 1;
-	}
-	if (flag == 1) save.pop_back();
-	while (it.more()) {
-		//std::cout << save.back() << " " << it.getNext() << std::endl;
-		if (save.back() != it.getNext())
-			return false;
-		save.pop_back();
-	}
-	return true;
-}
-
-template<typename T>
-bool palindromString(StringIterator<T> it) {
-	std::vector<T> save;
-	StringIterator<T> help = it;
-	bool flag = 0;
-	while (help.more()) {
-		save.push_back(it.getNext());
-		//std::cout << save.back() << " ";
-		help.getNext();
-		if (help.more()) help.getNext();
-		else flag = 1;
-	}
-	if (flag == 1) save.pop_back();
-	while (it.more()) {
-		//std::cout << save.back() << " " << it.getNext() << std::endl;
-		if (save.back() != it.getNext())
-			return false;
+		}
 		save.pop_back();
 	}
 	return true;
--- Stack-traversal-Homework3/main.cpp
+++ Stack-traversal-Homework3/main.cpp
@@ -14,7 +14,7 @@ void testListCount() {
 	list.push(0);
 	list.push(3);
 	ListIterator<int> it = list.begin();
-	std::cout << count<int>(it, 3) << std::endl;
+	std::cout << count<ListIterator<int>, int>(it, 3) << std::endl;
 }
 
 void testListDuplicates() {
@@ -30,7 +30,7 @@ void testListDuplicates() {
 	list.push(12);
 	list.push(11);
 	ListIterator<int> it = list.begin();
-	std::cout << duplicates<int>(it) << std::endl;
+	std::cout << duplicates<ListIterator<int>, int>(it) << std::endl;
 }
 
 void testListIsSorted() {
@@ -42,7 +42,7 @@ void testListIsSorted() {
 	list.push(34);
 	list.push(43);
 	ListIterator<int> it = list.begin();
-	std::cout << isSorted<int>(it) << std::endl;
+	std::cout << isSorted<ListIterator<int>, int>(it) << std::endl;
 }
 
 void testListPalindrom() {
@@ -55,55 +55,55 @@ void testListPalindrom() {
 	list.push(9);
 	list.push(1);
 	ListIterator<int> it = list.begin();
-	std::cout << palindrom(it) << std::endl;
+	std::cout << palindrom<ListIterator<int>, int>(it) << std::endl;
 }
 
 void testArrayCount() {
 	int arr[4] = { 3, 2, 3, 3 };
 	ArrayIterator<int> it(arr, 4);
-	std::cout << count<int>(it, 3) << std::endl;
+	std::cout << count<ArrayIterator<int>, int>(it, 3) << std::endl;
 }
 
 void testArrayDuplicates() {
 	int arr[10] = { 0, 2, 1, 8, 5, 1, 6, 3 };
 	ArrayIterator<int> it(arr, 8);
-	std::cout << duplicatesArray<int>(it) << std::endl;
+	std::cout << duplicates<ArrayIterator<int>, int>(it) << std::endl;
 }
 
 void testArrayIsSorted() {
 	int arr[4] = { 3, 5, 6, 7 };
 	ArrayIterator<int> it(arr, 4);
-	std::cout << isSorted<int>(it) << std::endl;
+	std::cout << isSorted<ArrayIterator<int>, int>(it) << std::endl;
 }
 
 void testArrayPalindrom() {
 	int arr[10] = { 3, 5, 6, 7 , 10,  10,  7, 6, 5, 3};
 	ArrayIterator<int> it(arr, 10);
-	std::cout << palindromArray<int>(it) << std::endl;
+	std::cout << palindrom<ArrayIterator<int>,int>(it) << std::endl;
 }
 
 void testStringCount() {
 	char *str = "baklava";
 	StringIterator<char> it(str);
-	std::cout << count<char>(it, 'a') << std::endl;
+	std::cout << count<StringIterator<char>, char>(it, 'a') << std::endl;
 }
 
 void testStringDuplicates() {
 	char *str = "baklava";
 	StringIterator<char> it(str);
-	std::cout << duplicatesString<char>(it) << std::endl;
+	std::cout << duplicates<StringIterator<char>, char>(it) << std::endl;
 }
 
 void testStringIsSorted() {
 	char *str = "ponmkgfedcbt";
 	StringIterator<char> it(str);
-	std::cout << isSorted<char>(it) << std::endl;
+	std::cout << isSorted<StringIterator<char>, char>(it) << std::endl;
 }
 
 void testStringPalindrom() {
 	char *str = "babaabab";
 	StringIterator<char> it(str);
-	std::cout << palindromString<char>(it) << std::endl;
+	std::cout << palindrom<StringIterator<char>, char>(it) << std::endl;
 }
 
 char function(unsigned int x) {
@@ -131,22 +131,22 @@ void testFunctionIterator() {
 }
 
 int main() {
-	//testListCount();
-	//testListDuplicates();
-	//testListIsSorted();
-	//testListPalindrom();
+	testListCount();
+	testListDuplicates();
+	testListIsSorted();
+	testListPalindrom();
 
-	//testArrayCount();
-	//testArrayDuplicates();
-	//testArrayIsSorted();
-	//testArrayPalindrom();
+	testArrayCount();
+	testArrayDuplicates();
+	testArrayIsSorted();
+	testArrayPalindrom();
 
-	//testStringCount();
-	//testStringDuplicates();
-	//testStringIsSorted();
-	//testStringPalindrom();
+	testStringCount();
+	testStringDuplicates();
+	testStringIsSorted();
+	testStringPalindrom();
 
-	testFunctionIterator();
+	//testFunctionIterator();
 
 	system("pause");
 	return 0;
