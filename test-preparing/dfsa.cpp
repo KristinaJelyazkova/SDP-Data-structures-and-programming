b73da68ae5e42c108530ed4401de617875a38d93
KristinaJelyazkova <thesecretgirlbls@abv.bg>
2018-01-17 23:27:50 +0200

    Add files via upload

--- test-preparing/dfsa.cpp
+++ test-preparing/dfsa.cpp
@@ -8,6 +8,20 @@
 
 class DFSA {
 private:
+	class StatesIterator {
+	public:
+		StatesIterator(DFSA const*, unsigned int);
+		unsigned int operator *() const;
+		StatesIterator& operator ++();
+		bool operator !=(const StatesIterator&) const;
+	private:
+		DFSA const *a;
+		unsigned int crrStateIndex;
+	};
+public:
+	StatesIterator& begin() const;
+	StatesIterator& end() const;
+private:
 	struct state;
 	struct transitionProxy {
 	private:
@@ -19,19 +33,37 @@ private:
 		operator unsigned int() const;
 	};
 	struct state {
+		friend class DFSA;
+	private:
 		unsigned int label;
 		bool isFinal;
 		bool isMarked;
 		std::map<char, unsigned int> transitions;
 		DFSA *a;
-
+	public:
 		state(DFSA *);
 		transitionProxy operator[](char);
+		unsigned int operator[](char) const;
+		bool final() const;
+	public:
+		class SymbolsIterator {
+		public:
+			SymbolsIterator(state const*, bool = false);
+			char operator*() const;
+			SymbolsIterator& operator++();
+			bool operator!=(const SymbolsIterator&) const;
+		private:
+			state *s;
+			std::map<char, unsigned int>::const_iterator transitionsIterator;
+		};
+		SymbolsIterator& begin() const;
+		SymbolsIterator& end() const;
 	};
 public:
 	DFSA(unsigned int = 0);
 	std::vector<char> getSymbolsFrom(unsigned int);
 	state& operator[](unsigned int);
+	const state& operator[](unsigned int) const;
 	void addTransition(unsigned int, char, unsigned int);
 	void setFinalState(unsigned int);
 	unsigned int toState(unsigned int, char);
@@ -45,7 +77,8 @@ private:
 	std::map<unsigned int, unsigned int> labelToIndex;
 
 	unsigned int indexOf(unsigned int);
-	unsigned int labelOf(unsigned int);
+	unsigned int labelOf(unsigned int) const;
+	unsigned int indexOf(unsigned int) const;
 
 	void printDotty(std::ostream&, state&);
 	bool readsWord(std::string, state&, int);
@@ -80,17 +113,26 @@ DFSA::state& DFSA::operator[](unsigned int label) {
 	return table[indexOf(label)];
 }
 
+const DFSA::state& DFSA::operator[](unsigned int label) const{
+	return table[indexOf(label)];
+}
+
 DFSA::state::state(DFSA *_a) : a(_a), isFinal(false){}
 
 DFSA::transitionProxy DFSA::state::operator[](char symbol) {
 	return transitionProxy(this, symbol);
 }
 
+unsigned int DFSA::state::operator[](char symbol) const{
+	return a->labelOf(transitions.at(symbol));
+}
+
+
 void DFSA::setFinalState(unsigned int label) {
 	table[indexOf(label)].isFinal = true;
 }
 
-unsigned int DFSA::labelOf(unsigned int index) {
+unsigned int DFSA::labelOf(unsigned int index) const{
 	return table[index].label;
 }
 
@@ -102,6 +144,10 @@ unsigned int DFSA::indexOf(unsigned int label) {
 		table.push_back(newState);
 	}
 	return labelToIndex[label];
+}
+
+unsigned int DFSA::indexOf(unsigned int label) const{
+	return labelToIndex.at(label);
 }
 
 void DFSA::addTransition(unsigned int labelFrom, char symbol, unsigned int labelTo) {
@@ -149,6 +195,66 @@ bool DFSA::readsWord(std::string s, state& crr, int index) {
 		return false;
 	}
 	return readsWord(s, table[crr.transitions[s[index]]], index + 1);
+}
+
+DFSA::StatesIterator::StatesIterator(DFSA const* _a, unsigned int _index)
+	: a(_a), crrStateIndex(_index) {}
+
+unsigned int DFSA::StatesIterator::operator *() const {
+	return a->table[crrStateIndex].label;
+}
+		
+DFSA::StatesIterator& DFSA::StatesIterator::operator++() {
+	crrStateIndex++;
+	return (*this);
+}
+		
+bool DFSA::StatesIterator::operator !=(const StatesIterator& other) const {
+	return a != other.a || crrStateIndex != other.crrStateIndex;
+}
+
+DFSA::StatesIterator& DFSA::begin() const {
+	return StatesIterator(this, 0);
+}
+
+DFSA::StatesIterator& DFSA::end() const {
+	return StatesIterator(this, table.size());
+}
+
+DFSA::state::SymbolsIterator::SymbolsIterator(state const*_s, bool end) {
+	if (end) {
+		s = nullptr;
+		transitionsIterator = s->transitions.end();
+	}
+	else {
+		s = _s;
+		transitionsIterator = s->transitions.begin();
+	}
+}
+
+char DFSA::state::SymbolsIterator::operator*() const {
+	return transitionsIterator->first;
+}
+
+DFSA::state::SymbolsIterator& DFSA::state::SymbolsIterator::operator++() {
+	transitionsIterator++;
+	return (*this);
+}
+
+bool DFSA::state::SymbolsIterator::operator!=(const SymbolsIterator& other) const {
+	return s != other.s || transitionsIterator != other.transitionsIterator;
+}
+
+DFSA::state::SymbolsIterator& DFSA::state::begin() const {
+	return SymbolsIterator(this);
+}
+
+DFSA::state::SymbolsIterator& DFSA::state::end() const{
+	return SymbolsIterator(this, true);
+}
+
+bool DFSA::state::final() const {
+	return isFinal;
 }
 
 #endif
